76c9d1c97dcec2a93992b5cfcde744bbf650be2c
caixiruo <3387494245@qq.com>
2025-06-07 01:46:27 +0800

    Add files via upload

--- 03-streamlit-walle-doraemon.py
+++ 03-streamlit-walle-doraemon.py
@@ -16,7 +16,7 @@ def load_model():
     """加载并缓存模型"""
     # Windows 路径兼容性处理
     temp = None
-    if sys.platform == "win32":
+    if sys.platform == "win64":
         # temp = pathlib.PosixPath
         pathlib.PosixPath = pathlib.WindowsPath
     
